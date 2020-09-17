7be38ea9b8194b68d26476df6e25a2bd921475fc
nhiroki0831 <nhiroki0831@gmail.com>
2020-09-17 17:15:44 +0900

    Rubocop実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,10 +29,10 @@ class ItemsController < ApplicationController
   def update
     item = Item.find(params[:id])
     if item.update(item_params)
-       redirect_to root_path
+      redirect_to root_path
     else
       render :new
-    end  
+    end
   end
 
   def destroy
