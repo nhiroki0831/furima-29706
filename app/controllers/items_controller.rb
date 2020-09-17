c6ea46977210aac65cef6737987313fedff011aa
nhiroki0831 <nhiroki0831@gmail.com>
2020-09-17 16:08:27 +0900

    rubocop 実施　エラーハンドリング追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,8 +28,11 @@ class ItemsController < ApplicationController
 
   def update
     item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to root_path
+    if item.update(item_params)
+       redirect_to root_path
+    else
+      render :new
+    end  
   end
 
   private
