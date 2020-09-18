6318db228d208cf772bbae7e210d0a092acb11c1
nhiroki0831 <nhiroki0831@gmail.com>
2020-09-18 14:31:12 +0900

    出品者以外の購入画面遷移先指定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :move_to_index, except: [:index]
 
   def index
+  end
+
+  private
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
