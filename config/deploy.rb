0a72b7b85e653e355ef14e0acf495f7a8ad9662e
nhiroki0831 <nhiroki0831@gmail.com>
2020-09-23 19:38:28 +0900

    デプロイ自動化　再修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'furima-29706'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:nhiroki/furima-29706.git'
+set :repo_url,  'git@github.com:nhiroki0831/furima-29706.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
