d56a9e25998860fed47d171743b8eee183de97e0
nhiroki0831 <nhiroki0831@gmail.com>
2020-10-01 12:39:04 +0900

    コメント機能 単体テストコード実施

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    
+    comment              { "コメントテスト" }
+    association   :user
+    association   :item
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
+describe Comment do
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
 
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'コメント機能' do
+    context 'コメント送信成功' do
+      it '必要な項目を全て入力済' do
+        expect(@comment).to be_valid
+      end
+    end
+    context 'コメント送信失敗' do
+      it 'コメントがないと送信できない' do
+        @comment.comment = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('コメントを入力してください')
+      end
+      it 'userが紐ついていないと登録できない' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('Userを入力してください')
+      end
+      it 'itemが紐ついていないと登録できない' do
+        @comment.item = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('Itemを入力してください')
+      end      
+    end
+  end
 end
