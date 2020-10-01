a964995d3413fc2b5eb42aa9314adcfd3275e638
nhiroki0831 <nhiroki0831@gmail.com>
2020-10-01 18:51:41 +0900

    commets アソシエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -26,7 +26,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   def self.search(search)
     if !search.nil?
